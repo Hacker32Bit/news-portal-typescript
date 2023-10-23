63caae43a9f7faad5a73e717971650a41d019d0a
Gektor Gevorgyan <gektor.gevorgyan@gmail.com>
2023-10-23 21:32:15 +0400

    Added styles for NewsCard

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <>
       <Header />
-      <NewsCard id={1} title={"title 1"} text={"text 1"} date={"2023-08-15"} image={"https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg"}/>
-      <NewsCard id={2} title={"title 2"} text={"text 2"} date={"2023-04-26"} image={"https://images.unsplash.com/photo-1575936123452-b67c3203c357?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"}/>
+      <NewsCard id={1} title={"title 1"} content={"text 1"} publishDate={"2023-08-15"} image={"https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg"}/>
+      <NewsCard id={2} title={"title 2"} content={"text 2"} publishDate={"2023-04-26"} image={"https://images.unsplash.com/photo-1575936123452-b67c3203c357?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"}/>
     </>
   );
 }
--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import styles from "./NewsCard.module.css"
-
-interface card {
-    id: number;
-    title: string;
-    text: string;
-    date: string;
-    image: string;
+import React from 'react'
+import styles from './NewsCard.module.css'
+interface NewsCardProps {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  publishDate: string;
 }
-
-export default function NewsCard({id, title, text, date, image}: card) {
-    return(
-        <div className={styles.card}>
-            <img src={image} alt="img"></img>
-            <h1>{title}</h1>
-            <p>{text}</p>
-            <button>Read more</button>
-            <span>{date}</span>
-        </div>
-    )
+const NewsCard: React.FC<NewsCardProps> = ({ id, title, content, image, publishDate }) => {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <p className={styles.date}>Published on: {publishDate}</p>
+      <a href='#' className={styles.read_more}>Read more</a>
+    </div>
+  )
 }
+export default NewsCard
